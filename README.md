24eca5dc915558ab0b75cc8b0681e2ca96781590
qbac <jakub.cyrklaf@gmail.com>
2020-05-06 15:10:28 +0200

    Connect to Database

---
+++ README.md
@@ -0,0 +1,5 @@
+# PcbPotwZam
+
+#This is a script cooperating with the Streamsoft Prestiż ERP system.
+
+Creates order confirmations from recipients in XML EDI format.
--- index.php
+++ index.php
@@ -3,5 +3,17 @@
 include_once('config.php');
 include_once('getPotw.php');
 
+try {
+    $conn = new PDO("firebird:host=".SERVER_FB.";dbname=".DATABASE_FB.";charset=UTF8", USER_DB_FB, PASS_DB_FB);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	//$conn -> query ('SET NAMES WIN1250');
+	//$conn -> query ('SET CHARACTER_SET WIN1250');
+    echo "<p>Connected successfully</p>";
+    }
+catch(PDOException $e)
+    {
+    echo "<p>Connection failed: " . $e->getMessage."</p>"();
+    }
 
 ?>
